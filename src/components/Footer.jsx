6fb73effaec601b697e1f5e2520f35801ec4d929
Daniel Cassin <dcassin2022@gmail.com>
2024-04-03 12:26:31 -0700

    first-commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { pageLinks, socialLinks } from "./../data";
+import PageLinks from "./PageLinks.jsx";
+import SocialLinks from "./SocialLinks.jsx";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+      {/* <ul className="footer-links">
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+      {/* <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+      <SocialLinks parentClass="footer-icons" itemClass="footer-icon" />
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span>
+        {new Date().getFullYear()} all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
